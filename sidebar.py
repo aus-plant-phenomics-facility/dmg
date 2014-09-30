ef2be912677f21cc707885e7091f8310c2c051d7
George Sainsbury <george.sainsbury@adelaide.edu.au>
2014-09-30 16:22:49 +0930

    pep8 except line lengths

--- sidebar.py
+++ sidebar.py
@@ -6,38 +6,38 @@ from os import listdir
 from os.path import isfile, join
 
 
-def printDir(mypath,c,local):
-	heading = ''
-	for i in range(0,c):
-		heading+= '#'
-	heading+=  mypath.split('/')[-1]
-	for i in range(0,c):
-		heading+=  '#'
-	print heading
-	onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) and f.endswith('.md') and not f.startswith('_')]
-	for p in onlyfiles:
-		printpath = mypath
-		if(local != 0):
-			printpath = 'aus-plant-phenomics-facility/dmg/wiki'
-		print '['+p.replace('-',' ')[:-3]+'](/'+printpath+'/'+p[:-3]+')\n'
+def printDir(directory, c, isLocal):
+    heading = ''
+    for i in range(0, c):
+        heading += '#'
+    heading += directory.split('/')[-1]
+    for i in range(0, c):
+        heading += '#'
+    print heading
+    onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f)) and f.endswith('.md') and not f.startswith('_')]
+    for p in onlyfiles:
+        printpath = directory
+        if(isLocal != 0):
+            printpath = 'aus-plant-phenomics-facility/dmg/wiki'
+        print '['+p.replace('-', ' ')[:-3]+'](/'+printpath+'/'+p[:-3]+')\n'
 
-	for item in os.listdir(mypath):
-		if os.path.isdir(os.path.join(mypath, item)) and not item.startswith('.'):
-				printDir(os.path.join(mypath, item),c+1,local)
+    for item in os.listdir(directory):
+        if os.path.isdir(os.path.join(directory, item)) and not item.startswith('.'):
+                printDir(os.path.join(directory, item), c+1, isLocal)
 
-sys.stdout = open('_Sidebar.md','w')
-mypath = ('.')
+sys.stdout = open('_Sidebar.md', 'w')
+directory = ('.')
 try:
-	printDir(mypath,1,-1)
+    printDir(directory, 1, -1)
 finally:
-	sys.stdout.close()
-call(['git','add','_Sidebar.md'])
-call(['git','commit', '-m"Updating Sidebar"'])
-call(['git','push', 'origin', 'master'])
-sys.stdout = open('_Sidebar.md','w')
+    sys.stdout.close()
+call(['git', 'add', '_Sidebar.md'])
+call(['git', 'commit', '-m"Updating Sidebar"'])
+call(['git', 'push', 'origin', 'master'])
+sys.stdout = open('_Sidebar.md', 'w')
 try:
-	printDir(mypath,1,0)
+    printDir(directory, 1, 0)
 finally:
-	sys.stdout.close()
-call(['git','add', '_Sidebar.md'])
-call(['git','commit', '-m"Updating Sidebar"'])
+    sys.stdout.close()
+call(['git', 'add', '_Sidebar.md'])
+call(['git', 'commit', '-m"Updating Sidebar"'])
